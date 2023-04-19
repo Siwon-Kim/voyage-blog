6b8a90714d31622d3dc7ffdbd3f850c1fdc0333c
Siwon-Kim <siwonkim1108@gmail.com>
2023-04-19 08:03:04 -0400

    final version for hw2 submission

--- schemas/index.js
+++ schemas/index.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose");
 
 const connect = () => {
 	mongoose
-		.connect("mongodb://localhost:27017/voyage_blog")
+		.connect("mongodb://localhost:27017/blog")
 		.catch((err) => console.log(err));
 };
 
