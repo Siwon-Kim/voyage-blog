c8c11394879e3603445ca1ef60b020f6a1e5fa68
Siwon-Kim <siwonkim1108@gmail.com>
2023-04-19 08:49:47 -0400

    final version

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const cookieParser = require("cookie-parser");
 const app = express();
-const port = 3001;
+const port = 3000;
 
 //Swagger
 const swaggerUi = require('swagger-ui-express')
--- schemas/index.js
+++ schemas/index.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose");
 
 const connect = () => {
 	mongoose
-		.connect("mongodb://localhost:27017/blog")
+		.connect("mongodb://localhost:27017/voyage-blog")
 		.catch((err) => console.log(err));
 };
 
